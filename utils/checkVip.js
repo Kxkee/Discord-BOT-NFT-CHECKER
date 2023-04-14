2a90a0994a32cf17481e4392bcf4f74d6b8e4b93
Kxkee <tom.cousin06@gmail.com>
2023-04-14 19:08:42 +0200

    .env

--- utils/checkVip.js
+++ utils/checkVip.js
@@ -2,7 +2,7 @@ const { createClient } = require('@supabase/supabase-js');
 
 
 const utils = {
-    supabase: createClient('https://mbsubdfkquqteagazpdj.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ic3ViZGZrcXVxdGVhZ2F6cGRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODAzNzY1NTMsImV4cCI6MTk5NTk1MjU1M30.U6QAWghxaVP2JTTROx6c_HI4K6VBdw3onsFzkbo6j1M'),
+    supabase: createClient(`${process.env.SUPABASE_URL}`, `${process.env.SUPABASE_KEY}`),
     checkVip: async (discordId) => {
     const {data: user, error} = await utils.supabase
         .from('discordUser')
